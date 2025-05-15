7adbd1a64332adfda8f469d1c7121aed1f2bc738
Veronica Mogolivets <mogolivetsveronika@gmail.com>
2025-05-15 08:55:56 +0000

    15.05

--- tree.cpp
+++ tree.cpp
@@ -16,10 +16,60 @@ struct tree_node {
     tree_node* left;
     tree_node* right;
 };
+void take_left(tree_node* start){ 
+	tree_node* current = start;
+	while (current != 0) {
+		std::cout << current->value << std::endl;
+		current = current->left;
+	}
+}
+void take_right(tree_node* push){
+	tree_node* now = push;
+	while (now != 0) {
+		std::cout << now->value << std::endl;
+		now = now->right;
+	}
+}
+void dfs(tree_node* start){
+	tree_node* current = start;
+	if(current == 0){
+		return ;
+	}
+	std::cout << current->value << std::endl;
+	dfs(current->left);
+	dfs(current->right);	
+ 
+}	
 int main() {
 	tree_node root;
+	tree_node l11, l12, l21, l22, l23, l24, l31, l32, l41;
 	root.value = 50;
-	tree_node l1;
+	root.left = &l11;
+	root.right = &l12;
+	l11.left = &l21;
+	l11.right = &l22;
+	l12.left = &l23;
+	l12.right = &l24;
+	l21.left = l21.right = 0;
+        l22.left = &l31;
+	l22.right = &l32;
+	l23.left = l23.right = 0;
+	l24.left = l24.right = 0;
+        l31.left = l31.right = 0;
+	l32.left = &l41;
+	l32.right = 0;
+	l41.left = l41.right = 0;
+
+	l11.value = 25;
+	l12.value = 71;
+	l21.value = 13;
+	l22.value = 26;
+	l23.value = 81;
+	l24.value = 33;
+	l31.value = 43;
+	l32.value = 15;
+	l41.value = 7;
+/*	tree_node l1;
 	root.left = &l1;
 	l1.value = 25;
 	tree_node l2;
@@ -54,5 +104,12 @@ int main() {
 	std::cout << root.left->left->value << "      " << root.right->right->value << "     " << (*l2.left).value << "      " << (*l3.right).value << std::endl;
 	std::cout << "    " << (*l3.left).value << "     " << (*l4.left).value << std::endl;
 	std::cout << "        " << (*l4.right).value << std::endl;
-        return 0;
+*/  
+       	std::cout << " ************************************************************************* " << std::endl;
+        take_left(&root);	
+        std::cout << " ************************************************************************* " << std::endl;
+        take_right(&l11);	
+        std::cout << " ************************************************************************* " << std::endl;
+	dfs(&root);
+       	return 0;
 }
